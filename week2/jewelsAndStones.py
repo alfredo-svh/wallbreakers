1bf560802c8cb3f2ad449ff4a6374756e80cccaf
Alfredo S <cruzazulini@hotmail.com>
2020-02-14 20:20:42 -0800

    first problem

---
+++ week2/jewelsAndStones.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        
+        res = 0
+        
+        for i in S:
+            if i in J:
+                res+=1
+                
+        return res
